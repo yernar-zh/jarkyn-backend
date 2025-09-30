ef648ebbffdd47246be12514f2fdd401ece6f889
Nebula <yernar.zharkynuly@gmail.com>
2025-09-30 11:07:03 +0500

    asdsadas

--- src/main/java/kz/jarkyn/backend/document/expense/repository/ExpenseRepository.java
+++
@@ -1,9 +0,0 @@
-package kz.jarkyn.backend.document.expense.repository;
-
-import kz.jarkyn.backend.core.repository.AppRepository;
-import kz.jarkyn.backend.document.expense.model.ExpenseEntity;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExpenseRepository extends AppRepository<ExpenseEntity> {
-}
